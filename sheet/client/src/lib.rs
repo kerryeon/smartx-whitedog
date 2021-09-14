2483716f6be8ecc0a538fc5058a11e1b3fbcaa87
Ho Kim <great.ho.kim@gmail.com>
2021-09-14 22:23:13 +0900

    Update

--- sheet/client/src/lib.rs
+++ sheet/client/src/lib.rs
@@ -1,16 +1,22 @@
 #[macro_use]
 extern crate anyhow;
 
-use std::{env, marker::PhantomData};
+use std::{
+    env, fmt,
+    marker::PhantomData,
+    ops::{self, Index},
+    str::FromStr,
+};
 
 use anyhow::Result;
-use google_sheets4::{api::SpreadsheetMethods, Sheets};
+use google_sheets4::{
+    api::{SpreadsheetMethods, ValueRange},
+    Sheets,
+};
 use hyper_rustls::HttpsConnector;
 use schemars::JsonSchema;
 use serde::{de::DeserializeOwned, Serialize};
 use yup_oauth2::ServiceAccountAuthenticator;
-
-pub use google_sheets4::api::ValueRange;
 
 /// Google Sheets를 제어 가능한 클라이언트입니다.
 pub struct SheetClient {
@@ -65,7 +71,7 @@ pub struct Spreadsheet<'a> {
 }
 
 impl<'a> Spreadsheet<'a> {
-    pub fn get_table<Field>(&self, fields_range: impl ToString) -> Result<Table<'_, Field>>
+    pub async fn get_table<Field>(&self, fields_range: impl ToString) -> Result<Table<'_, Field>>
     where
         Field: Serialize + DeserializeOwned + JsonSchema,
     {
@@ -76,13 +82,26 @@ impl<'a> Spreadsheet<'a> {
             .clone()
             .unwrap_or_else(|| "unknown field".to_string());
         match schema.object {
-            Some(object) => Ok(Table {
-                spreadsheet: self,
-                name,
-                fields: object.properties.into_iter().map(|(k, _)| k).collect(),
-                fields_range: fields_range.to_string(),
-                _table: PhantomData::<Field>::default(),
-            }),
+            Some(object) => {
+                let fields_range = fields_range.to_string();
+                let fields_struct: Vec<_> = object.properties.into_iter().map(|(k, _)| k).collect();
+                let fields_matrix = self.get(&fields_range).await?;
+                dbg!(&fields_matrix.size);
+                println!("{}", &fields_matrix.size);
+                dbg!(fields_matrix.data.len());
+
+                for row in &fields_matrix.data {
+                    dbg!(row);
+                }
+
+                Ok(Table {
+                    spreadsheet: self,
+                    name,
+                    fields: todo!(),
+                    fields_range,
+                    _table: PhantomData::<Field>::default(),
+                })
+            }
             None => bail!(
                 "field {} is not a struct (not supported: enum, union, ...)",
                 name
@@ -90,15 +109,24 @@ impl<'a> Spreadsheet<'a> {
         }
     }
 
-    async fn get(&self, range: &str) -> Result<ValueRange> {
+    async fn get(&self, range: &str) -> Result<Matrix> {
         let (_, ret) = self.client.values_get(&self.id, range).doit().await?;
-        Ok(ret)
+        Ok(Matrix {
+            size: ret.range.expect("range").parse()?,
+            data: ret.values.expect("values"),
+        })
     }
 
-    async fn update(&self, range: ValueRange) -> Result<()> {
-        let range_str = range.range.clone().ok_or_else(|| anyhow!("empty range"))?;
+    async fn update(&self, matrix: Matrix) -> Result<()> {
+        let range = matrix.size.to_string();
+        let value_range = ValueRange {
+            major_dimension: None,
+            range: Some(range.clone()),
+            values: Some(matrix.data),
+        };
+
         self.client
-            .values_update(range, &self.id, &range_str)
+            .values_update(value_range, &self.id, &range)
             .value_input_option("USER_ENTERED")
             .doit()
             .await?;
@@ -120,16 +148,157 @@ where
 {
     pub async fn get_rows(&self, length: Option<u32>) -> Result<Vec<Field>> {
         let range = self.spreadsheet.get(&self.fields_range).await?;
-        // let sheets = spreadsheet.sheets.expect("sheets");
-        // if sheets.len() != 1 {
-        //     bail!(
-        //         "0, 2 or more sheets are detected: {} => {}",
-        //         &self.name,
-        //         sheets.len()
-        //     );
-        // }
-        // dbg!(&sheets[0]);
         dbg!(range);
         todo!()
     }
+}
+
+#[derive(Clone, Debug)]
+pub struct Matrix {
+    size: MatrixSize,
+    data: Vec<Vec<String>>,
+}
+
+#[derive(Clone, Debug)]
+pub struct MatrixSize {
+    sheet: String,
+    start: MatrixIndex,
+    end: MatrixIndex,
+}
+
+impl FromStr for MatrixSize {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s.split("!");
+        let sheet = iter.next().unwrap();
+        let grid = iter
+            .next()
+            .ok_or_else(|| anyhow!("sheet name is required (ex: MySheet!A1:B2)"))?;
+        if let Some(_) = iter.next() {
+            bail!("malformed MatrixSize: {}", s);
+        }
+
+        let mut iter = grid.split(":");
+        let pos1 = iter.next().unwrap();
+        let pos2 = iter.next().unwrap_or(pos1);
+
+        let mut pos = [pos1.parse()?, pos2.parse()?];
+        pos.sort();
+        let [start, end] = pos;
+
+        Ok(Self {
+            sheet: sheet.to_string(),
+            start,
+            end,
+        })
+    }
+}
+
+impl fmt::Display for MatrixSize {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.sheet.fmt(f)?;
+        "!".fmt(f)?;
+        self.start.fmt(f)?;
+        if &self.start != &self.end {
+            ":".fmt(f)?;
+            self.end.fmt(f)?;
+        }
+        Ok(())
+    }
+}
+
+impl<'a> Index<&'a MatrixIndex> for MatrixSize {
+    type Output = String;
+
+    fn index(&self, index: &'a MatrixIndex) -> &Self::Output {
+        todo!()
+    }
+}
+
+impl MatrixSize {
+    pub fn width(&self) -> u16 {
+        self.end.col - self.start.col
+    }
+
+    pub fn height(&self) -> u32 {
+        self.end.row - self.start.row
+    }
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct MatrixIndex {
+    col: u16,
+    row: u32,
+}
+
+impl FromStr for MatrixIndex {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if !s.is_ascii() {
+            bail!("non-ascii code in MatrixIndex");
+        }
+
+        let mut col: u16 = 0;
+        let mut bytes = s.bytes();
+        for byte in &mut bytes {
+            if byte.is_ascii_alphabetic() {
+                col = col * Self::NUM_ALPHABETS + (byte.to_ascii_uppercase() - b'A') as u16;
+                if col >= 17_576 {
+                    bail!("columns over 'ZZZ' (17,576) are not supported");
+                }
+            } else {
+                let row: u32 = String::from_utf8([byte].into_iter().chain(bytes).collect())
+                    .unwrap()
+                    .parse()?;
+                if row == 0 {
+                    bail!("rows with zero index are not supported");
+                } else if row > 5_000_000 {
+                    bail!("rows over 5 million are not supported");
+                }
+                return Ok(Self { col, row: row - 1 });
+            }
+        }
+        bail!("malformed MatrixIndex: {}", s);
+    }
+}
+
+impl fmt::Display for MatrixIndex {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut col = self.col;
+        while {
+            (((col % Self::NUM_ALPHABETS) as u8 + b'A') as char).fmt(f)?;
+            col /= Self::NUM_ALPHABETS;
+            col > 0
+        } {}
+        (self.row + 1).fmt(f)?;
+        Ok(())
+    }
+}
+
+impl ops::Add for MatrixIndex {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            col: self.col + rhs.col,
+            row: self.row + rhs.row,
+        }
+    }
+}
+
+impl ops::Sub for MatrixIndex {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self {
+            col: self.col - rhs.col,
+            row: self.row - rhs.row,
+        }
+    }
+}
+
+impl MatrixIndex {
+    const NUM_ALPHABETS: u16 = (b'Z' - b'A' + 1) as u16;
 }
